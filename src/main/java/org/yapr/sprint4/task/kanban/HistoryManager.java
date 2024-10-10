24423bb44052b08a9ad474254feb63117234c54f
Boris <boris.z.077@gmail.com>
2024-10-10 23:55:23 +0300

    add project

--- src/main/java/org/yapr/sprint4/task/kanban/HistoryManager.java
+++ src/main/java/org/yapr/sprint4/task/kanban/HistoryManager.java
@@ -5,8 +5,12 @@ import org.yapr.sprint4.task.model.Task;
 import java.util.List;
 
 public interface HistoryManager {
+
     void add(Task task); // Добавляет задачу в историю
+
     List<Task> getHistory(); // Возвращает список просмотренных задач
+
     void remove(int id);
+
     void clear();  // Добавляем метод для очистки истории
 }
