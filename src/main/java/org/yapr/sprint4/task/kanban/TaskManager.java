2325916d0501db337415e3d04de2d3b64802c48d
Boris <boris.z.077@gmail.com>
2024-10-10 23:52:44 +0300

    add project

--- src/main/java/org/yapr/sprint4/task/kanban/TaskManager.java
+++ src/main/java/org/yapr/sprint4/task/kanban/TaskManager.java
@@ -8,21 +8,35 @@ import java.util.List;
 
 public interface TaskManager {
     List<Task> getAllTasks();
+
     Task getTaskById(int id);
+
     Epic getEpicById(int id);
+
     Subtask getSubtaskById(int id);
+
     void createTask(Task task);
+
     void createEpic(Epic epic);
+
     void createSubtask(Subtask subtask);
+
     void updateTask(Task task);
+
     void updateEpic(Epic epic);
+
     void updateSubtask(Subtask subtask);
+
     void deleteTaskById(int id);
+
     void deleteEpicById(int id);
+
     void deleteSubtaskById(int id);
+
     List<Subtask> getSubtasksForEpic(int epicId);
 
     void deleteAllTasks();
+
     List<Task> getHistory();  // Новый метод для получения истории просмотров
 }
 
