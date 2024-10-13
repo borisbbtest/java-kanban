63b4fd684ccef68434f04ba1a6ba767d30320165
Boris <boris.z.077@gmail.com>
2024-10-14 00:46:30 +0300

    add project

--- src/main/java/org/yapr/sprint4/task/kanban/FileBackedTaskManager.java
+++ src/main/java/org/yapr/sprint4/task/kanban/FileBackedTaskManager.java
@@ -145,7 +145,6 @@ public class FileBackedTaskManager extends InMemoryTaskManager {
                 throw new RuntimeException("Ошибка при парсинге приоритета: " + e.getMessage());
             }
         }
-
         return switch (type) {
             case "TASK" -> new Task(id, title, description, status,duration,startTime,priority);
             case "EPIC" -> new Epic(id, title, description);
